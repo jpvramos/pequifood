4a5e661b001b41ff98c9a8de1149253f6ee74617
jpvramos <joaopaulovieiraramos@gmail.com>
2020-09-13 23:50:49 -0300

    :card_file_box: feat: add Models

--- pequifood/app.py
+++ pequifood/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from pequifood.ext  import site
 from pequifood.ext import toolbar
 from pequifood.ext import config
+from pequifood.ext import db
 
 
 def create_app():
     app = Flask(__name__)
     config.init_app(app)
+    db.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
     
--- pequifood/ext/config/__init__.py
+++ pequifood/ext/config/__init__.py
@@ -1,5 +1,6 @@
 def init_app(app):
     app.config["SECRET_KEY"] = "abcd"
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///pequifood.db'
     
     if app.debug:
         app.config["DEBUG_TB_TEMPLATE_EDITOR_ENABLED"] = True
